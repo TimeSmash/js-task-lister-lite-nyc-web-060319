efda5c0f44e9078955c44463bb4a5cdd750754d8
TimeSmash <TimeSmash@github.com>
2019-07-16 16:14:03 -0400

    Made add task and remove first task  functionality

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+//This file is within <head>
+document.addEventListener("DOMContentLoaded", () => { //Once the page has been loaded, do everything in here
+  function whateverFunction(){
+    let newTask = document.querySelector('#new-task-description')
+    
+    let value = newTask.value
+    let var2 = document.querySelector("#tasks");
+    
+    let newListElement = document.createElement("li")
+    newListElement.innerText = value
+    var2.appendChild(newListElement)
+  }
+  
+  
+  document.querySelector("#create-task-form").addEventListener('submit', (eventObj) => {console.log(eventObj)
+    eventObj.preventDefault()
+    // let variable = document.querySelector("#new-task-description")
+    // console.log(eventObj.target)
+    whateverFunction()
+    console.log(newTask.value)
+  })
+ 
+  
+let removeButton =   document.createElement("button") 
+removeButton.innerText = "Remove last task!"
+removeButton.style = "font-size: 2em; font-family: 'Papyrus';"
+
+document.body.appendChild(removeButton)
+
+removeButton.addEventListener('click', function() {
+  //go into the ul, remove last li
+  let var2 = document.querySelector("#tasks");
+  var2.lastChild.remove()
+})
+
 });
+
+
+// form.addEventListener('submit', (eventObj) => {console.log(eventObj)})
+// LOG; eventObj
